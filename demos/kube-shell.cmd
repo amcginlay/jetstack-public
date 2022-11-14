c17222fb257e00a50db110d1eeea42112b166519
Alan McGinlay <amcginlay@users.noreply.github.com>
2022-11-14 18:40:59 +0000

    Delete kube-shell.cmd

--- demos/kube-shell.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-REM this script needs testing
-docker build -t kube-shell:1.0 ${PWD} # <-- can do better than this, want to force this to use the director the Dockerfile is in (which may not be $PWD)
-docker run -it --net host -v ${USERPROFILE}/.kube/:/root/.kube -v ${CD}:/work -w /work kube-shell:1.0 /bin/bash
